8af01d3c5c662386a2592604d69a853c8191314e
Ed Maphis <emaphis85@gmail.com>
2015-03-14 14:36:22 -0400

    Add testing function to geometry.erl

--- chap04/geometry.erl
+++ chap04/geometry.erl
@@ -1,7 +1,18 @@
+%%% a first module.
+%%% calculate the area of several geometric figures.
+
 -module(geometry).
--export([area/1]).
+-export([area/1, test/0]).
 
 area({rectangle, Width, Height}) ->
     Width * Height;
 area({square, Side}) ->
     Side * Side.
+
+
+%% testing function:
+
+test() ->
+    12  = area({rectangle, 3, 4}),
+    144 = area({square, 12}),
+    tests_worked.
