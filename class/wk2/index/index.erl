6abc123cb8ceae2acdc4abfbb8e06fc1c8db159c
Ed Maphis <emaphis85@gmail.com>
2017-03-02 16:49:37 -0500

    Start on index, ran into a coceptual road-block

--- class/wk2/index/index.erl
+++ class/wk2/index/index.erl
@@ -1,6 +1,25 @@
 -module(index).
 -export([get_file_contents/1,show_file_contents/1]).
+-export([index_file/1]).
 
+index_file(File) ->
+    Lines = get_file_contents(File),
+    Words = process_lines(Lines),
+    lists:reverse(Words).
+
+process_lines(Lines) -> process_lines(Lines, 0, []).
+process_lines([], _Count, Acc) -> Acc;
+process_lines([L|Ls], Count, Acc) ->
+    Next = Count+ 1,
+    process_lines(Ls, Next, process_line(L, Next, Acc)). 
+process_line(Line, Count, Acc) ->
+    Toks = string:tokens(Line, " ,.-"),
+    process_tokens(Toks, Count, Acc).
+
+process_tokens([], _, Acc) -> Acc;
+process_tokens([T|Ts], Count, Acc) ->
+    process_tokens(Ts, Count, [{Count,T} | Acc]).
+                                          
 % Used to read a file into a list of lines.
 % Example files available in:
 %   gettysburg-address.txt (short)
@@ -13,7 +32,7 @@
 get_file_contents(Name) ->
     {ok,File} = file:open(Name,[read]),
     Rev = get_all_lines(File,[]),
-lists:reverse(Rev).
+    lists:reverse(Rev).
 
 % Auxiliary function for get_file_contents.
 % Not exported.
@@ -32,7 +51,6 @@ get_all_lines(File,Partial) ->
 show_file_contents([L|Ls]) ->
     io:format("~s~n",[L]),
     show_file_contents(Ls);
- show_file_contents([]) ->
+show_file_contents([]) ->
     ok.    
      
-
