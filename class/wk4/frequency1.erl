f4c8525dd026fb63f3986822987e91c6878398bf
Ed Maphis <emaphis85@gmail.com>
2017-04-07 21:08:07 -0400

    Message passing homework

--- class/wk4/frequency1.erl
+++ class/wk4/frequency1.erl
@@ -7,12 +7,16 @@
 %%   (c) Francesco Cesarini and Simon Thompson
 
 %% 1.13 -- modified version
+%% 1. register a process name.
 
 -module(frequency1).
--export([init/0]).
+-export([start/0,init/0,loop/1]).
 
 %% These are the start functions used to create and
 %% initialize the server.
+
+start() ->
+  register(frequency1, spawn(frequency1, init, [])).
 
 init() ->
   Frequencies = {get_frequencies(), []},
@@ -30,7 +34,7 @@ loop(Frequencies) ->
       Pid ! {reply, Reply},
       loop(NewFrequencies);
     {request, Pid , {deallocate, Freq}} ->
-      NewFrequencies = deallocate(Frequencies, Freq),
+      NewFrequencies = deallocate(Frequencies, Freq, Pid),
       Pid ! {reply, ok},
       loop(NewFrequencies);
     {request, Pid, stop} ->
@@ -40,37 +44,47 @@ loop(Frequencies) ->
 %% The Internal Help Functions used to allocate and
 %% deallocate frequencies.
 
+%% check first if frequency is already allocated
 allocate({[], Allocated}, _Pid) ->
   {{[], Allocated}, {error, no_frequency}};
 allocate({[Freq|Free], Allocated}, Pid) ->
-  {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}}.
+  case lists:keyfind(Pid, 2, Allocated) of
+    false ->
+      {{Free, [{Freq, Pid}|Allocated]}, {ok, Freq}};
+    _ ->
+      {{[Freq|Free], Allocated}, {error, already_allocated}}    
+  end.
 
-deallocate({Free, Allocated}, Freq) ->
-  NewAllocated=lists:keydelete(Freq, 1, Allocated),
-  {[Freq|Free],  NewAllocated}.
-
+deallocate({Free, Allocated}, Freq, Pid) ->
+  case lists:keyfind(Pid, 2, Allocated) of
+    {Freq, Pid} ->
+       NewAllocated=lists:keydelete(Freq, 1, Allocated),
+          {[Freq|Free],  NewAllocated};
+    false  ->
+       {{Free, Allocated}, {error, no_allocation}}
+  end.
 
 %% example run:
-%% Freq = spawn(frequency1,init,[]).
-%% <0.49.0>
-%% Freq ! {request,self(),allocate}.
+%% frequency1:start().
+%% true
+%% frequency1 ! {request,self(),allocate}.
 %% {request,<0.37.0>,allocate}
 %% receive {reply,Msg} -> Msg end.
 %% {ok,10}
-%% Freq ! {request,self(),{deallocate,10}}.
+%% frequency1 ! {request,self(),{deallocate,10}}.
 %% {request,<0.37.0>,{deallocate,10}}
 %% f(Msg).
 %% ok.
 %% receive {reply,Msg} -> Msg end.
 %% ok.
-%% Freq ! {request,self(),allocate}.
+%% frequency1 ! {request,self(),allocate}.
 %% {request,<0.37.0>,allocate}
 %% f(Msg).
 %% ok.
 %% receive {reply,Msg} -> Msg end.
 %% {ok,10}
-%% Freq ! {request,self(),allocate}.
-%% {ok,11}
-%% Freq ! {request,self(),stop}.
+%% frequency1 ! {request,self(),allocate}.
+%% {error,already_allocated}
+%% frequency1 ! {request,self(),stop}.
 %% {request,<0.37.0>,stop}
 %%
