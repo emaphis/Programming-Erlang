8c6c677a68cf9fe746c1f0b360cb34aacf61624e
Ed Maphis <emaphis85@gmail.com>
2017-07-24 16:08:51 -0400

    Use of the 'if' statement.

--- intro/chp04/drop_2.erl
+++ intro/chp04/drop_2.erl
@@ -1,4 +1,5 @@
 %% chapter 4 - Using the return value from the case statent.
+%%  use of the 'if' statement.
 -module(drop_2).
 -export([fall_velocity/2]).
 
@@ -9,4 +10,18 @@ fall_velocity(Planemo, Distance) when Distance >= 0 ->
         moon  -> 1.6;
         mars  -> 3.71
     end,
-    math:sqrt(2 * Gravity * Distance).
+
+    Velocity = math:sqrt(2 * Gravity * Distance),
+
+    if
+        Velocity == 0  -> 'stable';
+        Velocity < 5   -> 'slow';
+        Velocity >= 5,  Velocity < 10  -> 'moving';
+        Velocity >= 10, Velocity < 20  -> 'fast';
+        Velocity >= 20 -> 'speedy'
+    end,
+
+    if
+        (Velocity > 40) -> io:format("Look out below!~n");
+        true  -> true
+    end.
